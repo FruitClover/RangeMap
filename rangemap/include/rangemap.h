ef4198541b63c740e13cf546c85af66a061cad2b
Michel K <fruitclover@gmail.com>
2018-07-02 23:56:22 +0300

    Add api to implement.

--- rangemap/include/rangemap.h
+++ rangemap/include/rangemap.h
@@ -2,13 +2,15 @@
 #define RANGEMAP_INCLUDE_H
 
 #include <map>
+#include <limits>
 
 namespace rangemap {
 
 class RangeMap {
 public:
-  typedef unsigned long size_type;
+  typedef uint64_t size_type;
   typedef size_t range_type;
+  static const size_type kUnknownSize = std::numeric_limits<size_type>::max();
 
   struct Entry {
     Entry(range_type type_, size_type size_) : type(type_), size(size_) {}
@@ -17,6 +19,8 @@ public:
   };
 
   void AddRange(range_type type, size_type addr, size_type size);
+  bool TryGetType(size_type addr, range_type* type, size_type* size) const;
+  bool IsCoverRange(size_type addr, size_type size) const;
 
 private:
   friend class RangeMapTest;
--- rangemap/src/rangemap.cc
+++ rangemap/src/rangemap.cc
@@ -2,7 +2,7 @@
 
 namespace rangemap {
 
-void RangeMap::AddRange(range_type type, size_type addr, size_type size){
+void RangeMap::AddRange(range_type type, size_type addr, size_type size) {
   map_.emplace(addr, Entry(type, size));
 }
 
