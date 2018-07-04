bc480c91118689a9bc6bb186bcb0156f31c532d3
Mikhail Kashkarov <m.kashkarov@partner.samsung.com>
2018-07-04 19:36:08 +0300

    Dev.

--- rangemap/src/rangemap.cc
+++ rangemap/src/rangemap.cc
@@ -32,6 +32,49 @@ namespace rangemap {
 // >  E--A-----B------------------C---------------------D2---J-------F------
 void RangeMap::AddRange(range_type type, size_type addr, size_type size) {
   map_.emplace(addr, Entry(type, size));
+  // First element whose key goes after addr (or return end())
+  auto it = map_.upper_bound(addr); // O(log n)
+}
+
+bool RangeMap::TryGetEntry(size_type addr, range_type *type,
+                           size_type *size) const {
+  return false;
+}
+
+bool RangeMap::IsRangeCovered(size_type addr, size_type size) const {
+  auto it = map_.upper_bound(addr);
+  if (it != map_.begin()) {
+    --it;
+  }
+
+  size_type beg = addr;
+  size_type end = addr + size;
+  assert(end >= size);
+  while(end > beg) {
+    beg += it->second.size;
+    if (it == map_.end())
+      return false;
+    // There is no element on the right side
+    // x..> covers [addr, addr + size]
+    if (it->second.size == kUnknownSize) {
+      return true;
+    }
+  }
+  return true;
+}
+
+RangeMap::size_type RangeMap::GetRangeEnd(Map_iterator it) {
+  if (it->second.size == kUnknownSize) {
+    return kUnknownSize;
+  }
+  size_type end = it->first + it->second.size;
+  // TODO: check overflow precisely
+  assert(end >= it->first);
+  return (it->first + it->second.size);
+}
+
+RangeMap::Map_iterator RangeMap::FindContaining(size_type addr) {
+  auto it = map_.lower_bound(addr);
 }
 
 } // namespace rangemap
